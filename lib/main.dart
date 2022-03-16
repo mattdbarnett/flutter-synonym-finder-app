e560454459ca52df838bc5e1dc98ac48319c97c6
Matt <mattdanielbarnett@gmail.com>
2022-03-16 23:23:20 +0000

    Added sliding up panel to main menu

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,15 +14,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -31,16 +23,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -48,94 +30,104 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
+    final _panelHeightOpen = MediaQuery.of(context).size.height * .80;
+    final _panelHeightClosed = MediaQuery.of(context).size.height * .10;
     return Scaffold(
-      body: Stack(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Container(
-                padding: const EdgeInsets.only(
-                  top: 50,
-                  left: 50,
-                ),
-                child: Align(
-                  alignment: Alignment.topRight,
-                  child: IconButton(
-                    padding: EdgeInsets.zero,
-                    icon: const Icon(
-                      Icons.settings,
-                      size: 40,
-                      color: Colors.black26,
+      body: SlidingUpPanel(
+        maxHeight: _panelHeightOpen,
+        minHeight: _panelHeightClosed,
+
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(40.0),
+          topRight: Radius.circular(40.0),
+        ),
+
+        panel: const Center(
+          child: Text("Test"),
+        ),
+
+        collapsed: const Icon(
+          Icons.keyboard_arrow_down,
+          size: 40,
+          color: Colors.black26,
+        ),
+
+        body: Stack(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(
+                    top: 50,
+                    left: 50,
+                  ),
+                  child: Align(
+                    alignment: Alignment.topRight,
+                    child: IconButton(
+                      padding: EdgeInsets.zero,
+                      icon: const Icon(
+                        Icons.settings,
+                        size: 40,
+                        color: Colors.black26,
+                      ),
+                      onPressed: () {
+                        // do something
+                      },
                     ),
-                    onPressed: () {
-                      // do something
-                    },
                   ),
                 ),
-              ),
-              const Spacer(),
-              Container(
-                padding: const EdgeInsets.only(
-                  top: 50,
-                  right: 50,
-                ),
-                child: Align(
-                  alignment: Alignment.topRight,
-                  child: IconButton(
-                    padding: EdgeInsets.zero,
-                    icon: const Icon(
-                      Icons.share,
-                      size: 40,
-                      color: Colors.black26,
+                const Spacer(),
+                Container(
+                  padding: const EdgeInsets.only(
+                    top: 50,
+                    right: 50,
+                  ),
+                  child: Align(
+                    alignment: Alignment.topRight,
+                    child: IconButton(
+                      padding: EdgeInsets.zero,
+                      icon: const Icon(
+                        Icons.share,
+                        size: 40,
+                        color: Colors.black26,
+                      ),
+                      onPressed: () {
+                        // do something
+                      },
                     ),
-                    onPressed: () {
-                      // do something
-                    },
-                  ),
-                ),
-              ),
-            ],
-          ),
-          Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: const [
-                Text(
-                  'Welcome',
-                  style: TextStyle(
-                    fontSize: 30.0,
-                    fontWeight: FontWeight.w800,
-                  ),
-                ),
-                Text(
-                  'Swipe up to begin',
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.black45,
-                    fontWeight: FontWeight.w400,
                   ),
                 ),
               ],
-            )
-          )
-        ],
+            ),
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: const [
+                  Text(
+                    'Welcome',
+                    style: TextStyle(
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.w800,
+                    ),
+                  ),
+                  Text(
+                    'Swipe up to begin',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.black45,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ],
+              )
+            ),
+          ],
+        )
       ),
     );
   }
